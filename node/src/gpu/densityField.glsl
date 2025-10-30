ab1b7066bfcc271c9bb729eeaee3ca31dc627d72
Logan Kirkland <111713273+LericDax@users.noreply.github.com>
2025-10-29 19:08:51 -0500

    Simplify node viewer scaffold with worker stub

--- node/src/gpu/densityField.glsl
+++
@@ -1,4 +0,0 @@
-// Placeholder shader for population density visualization.
-void main() {
-  // TODO: implement density field shader.
-}
--- node/src/util/messageBus.js
+++
@@ -1,8 +0,0 @@
-// Message bus helpers to standardize communication between main thread and worker.
-export function postToWorker(worker, message) {
-  worker?.postMessage(message);
-}
-
-export function handleWorkerMessage(event) {
-  console.debug('Received message from worker:', event.data);
-}
